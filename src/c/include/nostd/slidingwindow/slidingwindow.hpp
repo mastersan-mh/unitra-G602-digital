8c2075119e0149d7678c7e6a8d58afdec742754d
mastersan <mastersan_mh@list.ru>
2019-10-16 11:42:13 +0500

    (refs lib) const reverse iterator added.

--- src/c/include/nostd/slidingwindow/slidingwindow.hpp
+++ src/c/include/nostd/slidingwindow/slidingwindow.hpp
@@ -72,20 +72,20 @@ public:
 
         SlidingWindow * m_window;
         nostd::size_t m_index;  /* absolete index in array items[] */
-        nostd::size_t m_round_index;
+        nostd::size_t m_ring_index;
 
     public:
         enum Wherence
         {
-            SlidingWindow_const_iterator_BEGIN,
-            SlidingWindow_const_iterator_END
+            BEGIN,
+            END
         };
 
         const_iterator()
         {
             m_window = NULL;
             m_index = 0;
-            m_round_index = 0;
+            m_ring_index = 0;
         }
 
         const_iterator(const SlidingWindow &w, Wherence wherence)
@@ -94,16 +94,16 @@ public:
 
             switch(wherence)
             {
-                case SlidingWindow_const_iterator_BEGIN:
+                case BEGIN:
                 {
                     m_index = m_window->P_first();
-                    m_round_index = 0;
+                    m_ring_index = 0;
                     break;
                 }
-                case SlidingWindow_const_iterator_END:
+                case END:
                 {
-                    m_index = m_window->m_last;
-                    m_round_index = m_window->m_length;
+                    m_index = m_window->P_last();
+                    m_ring_index = m_window->m_length;
                     break;
                 }
             }
@@ -111,7 +111,7 @@ public:
 
         const value_type& operator*()
         {
-            if (m_round_index >= m_window->m_length)
+            if (m_ring_index >= m_window->m_length)
             {
                 FATAL(NOSTD_ERR_OUT_OF_RANGE);
             }
@@ -119,7 +119,7 @@ public:
         }
         const value_type* operator->()
         {
-            if (m_round_index >= m_window->m_length)
+            if (m_ring_index >= m_window->m_length)
             {
                 FATAL(NOSTD_ERR_OUT_OF_RANGE);
             }
@@ -128,7 +128,7 @@ public:
 
         const value_type* operator++()
         {
-            if(m_round_index >= m_window->m_length)
+            if(m_ring_index >= m_window->m_length)
             {
                 return nullptr;
             }
@@ -140,14 +140,14 @@ public:
                 m_index = 0;
             }
 
-            ++m_round_index;
+            ++m_ring_index;
 
             return tmp;
         }
 
         bool operator==(const const_iterator& other) const
         {
-            return m_round_index == other.m_round_index;
+            return m_ring_index == other.m_ring_index;
         }
 
         bool operator!=(const const_iterator& other) const
@@ -157,23 +157,138 @@ public:
 
         bool operator<(const const_iterator& other) const
         {
-            return m_round_index < other.m_round_index;
+            return m_ring_index < other.m_ring_index;
         }
 
         bool operator>(const const_iterator& other) const
         {
-            return m_round_index > other.m_round_index;
+            return m_ring_index > other.m_ring_index;
+        }
+    };
+
+    class reverse_const_iterator
+    {
+    private:
+
+        SlidingWindow * m_window;
+        nostd::size_t m_index;  /* absolete index in array items[] */
+        nostd::size_t m_ring_index;
+
+    public:
+        enum Wherence
+        {
+            BEGIN,
+            END
+        };
+
+        reverse_const_iterator()
+        {
+            m_window = NULL;
+            m_index = 0;
+            m_ring_index = 0;
+        }
+
+        reverse_const_iterator(const SlidingWindow &w, Wherence wherence)
+        {
+            m_window = (SlidingWindow*)&w;
+
+            switch(wherence)
+            {
+                case BEGIN:
+                {
+                    m_index = m_window->P_last();
+                    m_ring_index = 0;
+                    break;
+                }
+                case END:
+                {
+                    m_index = m_window->P_first();
+                    m_ring_index = m_window->m_length;
+                    break;
+                }
+            }
+        }
+
+        const value_type& operator*()
+        {
+            if (m_ring_index >= m_window->m_length)
+            {
+                FATAL(NOSTD_ERR_OUT_OF_RANGE);
+            }
+            return m_window->m_items[m_index];
+        }
+        const value_type* operator->()
+        {
+            if (m_ring_index >= m_window->m_length)
+            {
+                FATAL(NOSTD_ERR_OUT_OF_RANGE);
+            }
+            return m_window->m_items[m_index];
+        }
+
+        const value_type* operator--()
+        {
+            if(m_ring_index >= m_window->m_length)
+            {
+                return nullptr;
+            }
+
+
+            value_type * tmp = &m_window->m_items[m_index];
+
+            if(m_index == 0)
+            {
+                m_index = m_window->m_length - 1;
+            }
+            else
+            {
+                --m_index;
+            }
+
+            ++m_ring_index;
+
+            return tmp;
+        }
+
+        bool operator==(const reverse_const_iterator& other) const
+        {
+            return m_ring_index == other.m_ring_index;
+        }
+
+        bool operator!=(const reverse_const_iterator& other) const
+        {
+            return !(*this == other);
+        }
+
+        bool operator<(const reverse_const_iterator& other) const
+        {
+            return m_ring_index < other.m_ring_index;
+        }
+
+        bool operator>(const reverse_const_iterator& other) const
+        {
+            return m_ring_index > other.m_ring_index;
         }
     };
 
     const_iterator cbegin() noexcept
     {
-        return const_iterator(*this, const_iterator::SlidingWindow_const_iterator_BEGIN);
+        return const_iterator(*this, const_iterator::BEGIN);
     }
 
     const_iterator cend() noexcept
     {
-        return const_iterator(*this, const_iterator::SlidingWindow_const_iterator_END);
+        return const_iterator(*this, const_iterator::END);
+    }
+
+    reverse_const_iterator rcbegin() noexcept
+    {
+        return reverse_const_iterator(*this, reverse_const_iterator::BEGIN);
+    }
+
+    reverse_const_iterator rcend() noexcept
+    {
+        return reverse_const_iterator(*this, reverse_const_iterator::END);
     }
 
     value_type& at(nostd::size_t i)
@@ -192,35 +307,10 @@ public:
 
     value_type& operator[](nostd::size_t i)
     {
-        nostd::size_t index = P_first() + i;
+        nostd::size_t index = P_last() - i;
         while(index >= m_length) index -= m_length;
         return m_items[index];
     }
-
-#if 0
-    bool operator!=(SlidingWindow const& other) const;
-    bool operator==(SlidingWindow const& other) const; //need for BOOST_FOREACH
-    typename OwnIterator::reference operator*() const;
-    OwnIterator& operator++();
-
-    /* i++ */
-    SlidingWindow operator++()
-    {
-        return SlidingWindow();
-    }
-
-    /* ++i */
-    SlidingWindow & operator++()
-    {
-        return SlidingWindow();
-    }
-
-    void cbegincc()
-    {
-std::array<int> a;
-a.begin();
-    }
-#endif
 
 private:
 
@@ -228,6 +318,11 @@ private:
     {
         nostd::size_t tmp_first = m_last + 1;
         return (tmp_first >= m_length) ? 0 : tmp_first;
+    }
+
+    nostd::size_t P_last() noexcept
+    {
+        return m_last;
     }
 
     nostd::size_t m_length;
